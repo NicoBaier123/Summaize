b0b6cf4f377ddc7f502b77c93d97f62a9899e43a
okh-mzny <60819623+okh-mzny@users.noreply.github.com>
2024-10-16 19:16:26 +0200

    casing change because i dont know

--- summaize-frontend/src/components/NavBar.vue
+++ summaize-frontend/src/components/NavBar.vue
@@ -1,17 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <router-link class="navbar-brand" to="/">Logo</router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
