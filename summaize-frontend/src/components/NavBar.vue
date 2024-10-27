d658e17519a3e8afdfa106f33a6bd82c3c2f507f
CookieClickr <louisenrico.hauger@gmail.com>
2024-10-27 16:58:13 +0100

    Aboutpage altered

--- summaize-frontend/src/components/NavBar.vue
+++ summaize-frontend/src/components/NavBar.vue
@@ -8,7 +8,9 @@
     >
       <!-- Linke Seite mit den Links -->
       <div class="d-flex align-items-center">
-        <router-link class="navbar-brand nav-item" to="/">Logo</router-link>
+        <router-link class="navbar-brand nav-item" to="/"
+          >SummerAize</router-link
+        >
         <router-link class="nav-link nav-item" to="/about">About</router-link>
         <router-link class="nav-link nav-item" to="/admin">Admin</router-link>
       </div>
